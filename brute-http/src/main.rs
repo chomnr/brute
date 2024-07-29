0875af2690c8a25e362c3ac52536753c6daa42b4
chomnr <zeljkovranjes@outlook.com>
2024-07-29 17:46:45 -0400

    removed pool and ipinfo from main

--- brute-http/src/main.rs
+++ brute-http/src/main.rs
@@ -3,40 +3,21 @@ mod http;
 mod model;
 mod error;
 
-use std::{env::var, sync::Arc};
-
 use actix::{Actor, System};
 use anyhow::Result;
 
 use brute::{BruteConfig, BruteSystem};
 use http::serve;
-use ipinfo::{IpInfo, IpInfoConfig};
-use parking_lot::Mutex;
-use sqlx::postgres::PgPoolOptions;
 
 fn main() -> Result<()>  {
     let system = System::new();
     system.block_on(async {
-        // brute p1
+        // brute
         let brute_config = BruteConfig::default();
+        let brute_system = BruteSystem::new_brute(brute_config).await.start();
 
-        // sqlx
-        let pg_pool = PgPoolOptions::new().max_connections(200)
-            .connect(&brute_config.conn_string).await.unwrap();
-
-        // ipinfo
-        let ipinfo_config = IpInfoConfig {
-            token: Some(brute_config.ipinfo_token.clone()),
-            ..Default::default()
-        };
-        let ipinfo = IpInfo::new(ipinfo_config).unwrap();
-
-        // brute p2 /w actor
-        let brute_system = BruteSystem::new(pg_pool, Arc::new(Mutex::new(ipinfo)));
-        let actor = brute_system.start();
-        
         // axum
-        serve(actor).await.unwrap();
+        serve(brute_system).await.unwrap();
     });
     Ok(())
 }
