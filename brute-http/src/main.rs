b8ad29daec4499ad720913e6f42a4f2ac4c14125
chomnr <zeljkovranjes@outlook.com>
2024-07-28 21:59:31 -0400

    add bitflags

--- brute-http/src/main.rs
+++ brute-http/src/main.rs
@@ -1,7 +1,23 @@
-mod http;
+use flags::QueryFlags;
 
-fn main() {
-    println!("Hello, world!");
+mod http;
+mod flags;
+
+use anyhow::Result;
+
+pub struct Brute {
+
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    //println!("Hello, world!");
+    Ok(())
 }
 
 // Metrics::add("derive::33")
+
+// notes.
+// if the same ip hits the server then just reuse the ipinfo data
+// if the same ip hits an hour later then regrab the data this is to
+// keep the most accurate data.
