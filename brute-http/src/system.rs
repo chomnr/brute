eb2de261acc54975bf88678b2988bf1b7f057369
chomnr <zeljkovranjes@outlook.com>
2024-07-29 22:50:27 -0400

    added brutesystem w/ actix actor.

---
+++ brute-http/src/system.rs
@@ -0,0 +1,62 @@
+use std::sync::Arc;
+
+use actix::{Actor, Context};
+use ipinfo::IpInfo;
+use parking_lot::Mutex;
+use sqlx::{Pool, Postgres};
+
+//////////////////////
+// SYSTEM /w ACTOR //
+////////////////////
+
+pub struct BruteSystem {
+    /// PostgreSQL connection pool.
+    pub db_pool: Pool<Postgres>,
+
+    /// IP info client with shared access.
+    pub ipinfo_client: Arc<Mutex<IpInfo>>,
+}
+
+impl BruteSystem {
+    /// Creates a new instance of `BruteSystem`.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the provided database pool is closed.
+    ///
+    /// # Examples
+    ///
+    /// ```ignore
+    /// // Create the PostgreSQL connection pool
+    /// let brute_config = BruteConfig::default();
+    ///
+    /// // Create an instance of BruteSystem
+    /// let brute_system = BruteSystem::new(brute_config); // as an actor you will append .start() at the end.s
+    /// ```
+    pub async fn new_brute(pg_pool: Pool<Postgres>, ipinfo_client: IpInfo) -> Self {
+        Self {
+            db_pool: pg_pool,
+            ipinfo_client: Arc::new(parking_lot::Mutex::new(ipinfo_client)),
+        }
+    }
+}
+
+impl Actor for BruteSystem {
+    type Context = Context<Self>;
+}
+
+/*
+impl Handler<Request<Individual>> for BruteSystem {
+    type Result = ();
+    
+    fn handle(&mut self, msg: Request<Individual>, ctx: &mut Self::Context) -> Self::Result {
+        let pool = self.db_pool.clone();
+        let ipinfo = self.ipinfo_client.clone();
+
+        let fut = Box::pin(async move { println!("requested recieved") });
+
+        //ctx.spawn(fut.into_actor(self));
+        Ok(StatusCode::OK)
+    }
+}
+*/
