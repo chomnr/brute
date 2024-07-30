0bc7fb514c1bff99a8ea99d706ab3fc2e61f126d
chomnr <zeljkovranjes@outlook.com>
2024-07-30 04:16:45 -0400

    added patched openssh.

---
+++ patches/openssh-9.3-janky/version.h
@@ -0,0 +1,6 @@
+/* $OpenBSD: version.h,v 1.97 2023/03/15 21:19:57 djm Exp $ */
+
+#define SSH_VERSION	"OpenSSH_9.3 BruteExpose Edition "
+
+#define SSH_PORTABLE	"p1"
+#define SSH_RELEASE	SSH_VERSION SSH_PORTABLE
