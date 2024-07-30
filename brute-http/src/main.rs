ef87501791700c0432748c3b0649e906b1483ecc
chomnr <zeljkovranjes@outlook.com>
2024-07-29 22:37:20 -0400

    modified main according to the rest of the files

--- brute-http/src/main.rs
+++ brute-http/src/main.rs
@@ -1,27 +1,59 @@
-mod brute;
-mod http;
-mod error;
-
-use actix::{Actor, System};
+use actix::System;
 use anyhow::Result;
+use brute_http::{config::Config, http::serve};
+use clap::Parser;
+use ipinfo::IpInfoConfig;
+use log::info;
+use sqlx::
+    postgres::PgPoolOptions
+;
 
-use brute::{BruteConfig, BruteSystem};
-use http::serve;
+fn main() -> Result<()> {
+    // Load environment variables from .env file.
+    // Fails if .env file not found, not readable or invalid.
+    dotenvy::dotenv()?;
 
-fn main() -> Result<()>  {
+    // Start the logger based on environment configuration settings.
+    env_logger::builder()
+        .filter_module("async_io", log::LevelFilter::Off)
+        .filter_module("async_std", log::LevelFilter::Off)
+        .filter_module("polling", log::LevelFilter::Off)
+        .filter_module("tracing", log::LevelFilter::Off)
+        .filter_module("sqlx", log::LevelFilter::Off)
+        .init();
+
+    // Parse command-line arguments and environment variables to
+    // create a Config instance, loading configuration settings for the application.
+    let config = Config::parse();
+
+    // Create a new Actix system instance to manage
+    // the Actix actor framework environment for the application.
     let system = System::new();
+
+    info!("Initializing...");
     system.block_on(async {
-        // brute
-        let brute_config = BruteConfig::default();
-        let brute_system = BruteSystem::new_brute(brute_config).await.start();
+        // Create a new connection pool for PostgreSQL with a maximum of
+        // 500 connections Connect to the database using the URL provided in the configuration.
+        let db: sqlx::Pool<sqlx::Postgres> = PgPoolOptions::new()
+            .max_connections(500)
+            .connect(&config.database_url)
+            .await
+            .unwrap();
+
+        // Create an instance of `IpInfoConfig` with the
+        // provided token and default settings for other fields.
+        let ipinfo_config = IpInfoConfig {
+            token: Some(config.ipinfo_token.to_string()),
+            ..Default::default()
+        };
         
-        // axum
-        serve(brute_system).await.unwrap();
+        // Ensure the database is migrated correctly on startup.
+        sqlx::migrate!("..\\migrations\\").run(&db).await.unwrap();
+
+        info!("Migration process completed successfully.");
+
+        // Start listening.
+        serve().await.unwrap();
     });
     Ok(())
 }
-
-// notes.
-// if the same ip hits the server then just reuse the ipinfo data
-// if the same ip hits an hour later then regrab the data this is to
-// keep the most accurate data.
