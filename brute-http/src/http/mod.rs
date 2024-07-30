1729c8ad75d6728775334f398f62c07ff86a11ac
chomnr <zeljkovranjes@outlook.com>
2024-07-29 22:37:27 -0400

    adjust mod.rs

--- brute-http/src/http/mod.rs
+++ brute-http/src/http/mod.rs
@@ -1,27 +1,21 @@
-use std::{env::var, net::SocketAddr};
+use std::net::SocketAddr;
 
-use actix::Addr;
 use anyhow::Context;
 use axum::{Extension, Router};
-use get::get_router;
-use post::post_router;
+use dotenvy::var;
 use tokio::net::TcpListener;
 use tower_http::{limit::RequestBodyLimitLayer, trace::TraceLayer};
 
-use crate::brute::BruteSystem;
 
-mod get;
-mod post;
-
-pub async fn serve(brute: Addr<BruteSystem>) -> anyhow::Result<()> {
+pub async fn serve() -> anyhow::Result<()> {
     // environment variables
     // let bearer_token = var("BRUTE_BEARER_TOKEN")?;
-    let listen_on = var("BRUTE_LISTEN_ADDRESS")?;
+    let listen_on = var("LISTEN_ADDRESS")?;
 
     // router
     let app = api_router()
         // Add a layer to access the instance of Brute
-        .layer(Extension(brute))
+        //.layer(Extension(brute))
         // Add a layer to limit the size of the request body to 60 KB
         .layer(RequestBodyLimitLayer::new(60 * 1024))
         // Add a layer for tracing HTTP requests
@@ -39,5 +33,5 @@ pub async fn serve(brute: Addr<BruteSystem>) -> anyhow::Result<()> {
 
 fn api_router() -> Router {
     let router = Router::new();
-    router.merge(get_router()).merge(post_router())
+    router
 }
