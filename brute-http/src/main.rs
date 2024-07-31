758af219399d3705d52d8fdb5424c9a7a9611b16
chomnr <zeljkovranjes@outlook.com>
2024-07-31 19:24:25 -0400

    ... i forgot to call .run()

--- brute-http/src/main.rs
+++ brute-http/src/main.rs
@@ -1,15 +1,17 @@
-use std::path::{Path, PathBuf};
-use actix::{System, Actor};
+use actix::{Actor, System};
 use anyhow::Result;
 use axum_server::tls_rustls::RustlsConfig;
-use brute_http::{config::Config, http::{serve, serve_tls}, system::BruteSystem};
+use brute_http::{
+    config::Config,
+    http::{serve, serve_tls},
+    system::BruteSystem,
+};
 use clap::Parser;
 use dotenvy::var;
 use ipinfo::{IpInfo, IpInfoConfig};
 use log::info;
-use sqlx::{migrate::Migrator, 
-    postgres::PgPoolOptions}
-;
+use sqlx::{migrate::Migrator, postgres::PgPoolOptions};
+use std::path::{Path, PathBuf};
 
 fn main() -> Result<()> {
     // Load environment variables from .env file.
@@ -22,7 +24,7 @@ fn main() -> Result<()> {
         .filter_module("async_std", log::LevelFilter::Off)
         .filter_module("polling", log::LevelFilter::Off)
         .filter_module("tracing", log::LevelFilter::Off)
-        .filter_module("sqlx", log::LevelFilter::Off)
+        //.filter_module("sqlx", log::LevelFilter::Off)
         .filter_module("tower_http", log::LevelFilter::Off)
         .filter_module("tower::buffer::worker", log::LevelFilter::Off)
         .filter_module("tower::buffer::service", log::LevelFilter::Off)
@@ -46,17 +48,30 @@ fn main() -> Result<()> {
             .connect(&config.database_url)
             .await
             .unwrap();
-        
-        // Ensure the database is migrated correctly on startup. 
+
+        // Ensure the database is migrated correctly on startup.
         // Docker's doesn't support this sort of path so we need
         // to improvise.
-        let is_docker: bool = var("RUNNING_IN_DOCKER").unwrap_or(false.to_string()).parse().unwrap();
+        let is_docker: bool = var("RUNNING_IN_DOCKER")
+            .unwrap_or(false.to_string())
+            .parse()
+            .unwrap();
         if !is_docker {
             //sqlx::migrate!("..\\migrations\\").run(&db).await.unwrap();
-            Migrator::new(Path::new("..\\migrations\\")).await.unwrap();
+            Migrator::new(Path::new("..\\migrations\\"))
+                .await
+                .unwrap()
+                .run(&db)
+                .await
+                .unwrap();
             info!("Migration process completed successfully.");
         } else {
-            Migrator::new(Path::new("migrations")).await.unwrap();
+            Migrator::new(Path::new("migrations"))
+                .await
+                .unwrap()
+                .run(&db)
+                .await
+                .unwrap();
             info!("(Docker) Migration process completed successfully.");
         }
 
@@ -85,7 +100,7 @@ fn main() -> Result<()> {
         .unwrap();
 
         // Start listening.
-         //serve(brute_actor).await.unwrap(); // non tls
+        //serve(brute_actor).await.unwrap(); // non tls
         serve_tls(brute_actor, config).await.unwrap(); // tls
     });
     Ok(())
