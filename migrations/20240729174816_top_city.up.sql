2c57a98a779ab41ffdb10423e549fe60e84b4503
chomnr <zeljkovranjes@outlook.com>
2024-08-01 12:11:01 -0400

    schema now reflects model.

--- migrations/20240729174816_top_city.up.sql
+++ migrations/20240729174816_top_city.up.sql
@@ -1,5 +1,7 @@
 -- Add up migration script here
 CREATE TABLE top_city (
-    city VARCHAR(255) PRIMARY KEY,
-    amount INTEGER NOT NULL DEFAULT 0
+    city VARCHAR(255) NOT NULL,
+    country VARCHAR(255) NOT NULL,
+    amount INTEGER NOT NULL DEFAULT 0,
+    CONSTRAINT unique_city_country UNIQUE (city, country)
 );
--- migrations/20240729174821_top_region.up.sql
+++ migrations/20240729174821_top_region.up.sql
@@ -1,5 +1,7 @@
 -- Add up migration script here
 CREATE TABLE top_region (
-    region VARCHAR(255) PRIMARY KEY,
-    amount INTEGER NOT NULL DEFAULT 0
+    region VARCHAR(255) NOT NULL,
+    country VARCHAR(255) NOT NULL,
+    amount INTEGER NOT NULL DEFAULT 0,
+    CONSTRAINT unique_region_country UNIQUE (region, country)
 );
