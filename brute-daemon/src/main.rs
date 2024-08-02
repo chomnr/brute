c360a561c9acd50ed748abb47499023a1b580b22
chomnr <zeljkovranjes@outlook.com>
2024-08-02 19:02:03 -0400

    added function to create and remove pid.

--- brute-daemon/src/main.rs
+++ brute-daemon/src/main.rs
@@ -1,7 +1,11 @@
 #[cfg(target_os = "linux")]
 use std::fs::File;
+use std::{fs::File, process};
 #[cfg(target_os = "linux")]
 use daemonize::Daemonize;
+
+use std::io::{self, Write};  // Import the Write trait here
+
 
 use protocol::ssh::listen_on_ssh;
 
@@ -11,6 +15,7 @@ mod endpoint;
 #[cfg(target_os = "windows")]
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    
     dotenvy::dotenv().unwrap();
     listen_on_ssh().await?;
     Ok(())
@@ -19,21 +24,59 @@ async fn main() -> anyhow::Result<()> {
 #[cfg(target_os = "linux")]
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    println!("Initializing!");
     dotenvy::dotenv().unwrap();
+
+    if let Err(e) = create_pid_file() {
+        eprintln!("Failed to create PID file: {}", e);
+        return Err(anyhow::anyhow!("Failed to create PID file"));
+    }
+
+    let stdout = File::create("/tmp/bruted.out").unwrap();
+    let stderr = File::create("/tmp/bruted.err").unwrap();
+
     let daemonize = Daemonize::new()
         .pid_file("/tmp/bruted.pid")
         .chown_pid_file(true)
         .umask(0o777)
-        .group("brute")
+        .group("bruted")
         .working_directory("/tmp")
-        .stdout(File::create("/tmp/brute_daemon.stdout").unwrap())
-        .stderr(File::create("/tmp/brute_daemon.stderr").unwrap()); 
+        .stdout(stdout)
+        .stderr(stderr);
+
     match daemonize.start() {
         Ok(_) => {
             println!("Success, daemonized");
-            listen_on_ssh().await?;
+            if let Err(e) = listen_on_ssh().await {
+                eprintln!("Error listening on SSH: {}", e);
+                // Remove PID file if SSH server fails to start
+                let _ = remove_pid_file();
+                return Err(anyhow::anyhow!("Error listening on SSH"));
+            }
         },
-        Err(e) => eprintln!("Error, {}", e),
+        Err(e) => {
+            eprintln!("Error daemonizing: {}", e);
+            // Attempt to remove PID file if daemonization fails
+            let _ = remove_pid_file();
+            return Err(anyhow::anyhow!("Error daemonizing"));
+        },
     }
+
+    // Remove PID file on successful shutdown
+    let _ = remove_pid_file();
+
+    Ok(())
+}
+#[cfg(target_os = "linux")]
+
+fn remove_pid_file() -> anyhow::Result<()> {
+    std::fs::remove_file("/tmp/bruted.pid")?;
+    Ok(())
+}
+#[cfg(target_os = "linux")]
+fn create_pid_file() -> std::io::Result<()> {
+    let pid = process::id();
+    let mut file = File::create("/tmp/bruted.pid")?;
+    write!(file, "{}", pid)?;
     Ok(())
 }
