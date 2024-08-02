ccedb27fc105fe1b2346f5688763f229d395aba1
chomnr <zeljkovranjes@outlook.com>
2024-08-01 22:42:48 -0400

    readded filter

--- brute-http/src/main.rs
+++ brute-http/src/main.rs
@@ -1,11 +1,7 @@
 use actix::Actor;
 use anyhow::Result;
 use axum_server::tls_rustls::RustlsConfig;
-use brute_http::{
-    config::Config,
-    http::{serve, serve_tls},
-    system::BruteSystem,
-};
+use brute_http::{config::Config, http::serve_tls, system::BruteSystem};
 use clap::Parser;
 use dotenvy::var;
 use ipinfo::{IpInfo, IpInfoConfig};
@@ -17,19 +13,19 @@ use std::path::{Path, PathBuf};
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Load environment variables from .env file
     dotenvy::dotenv().unwrap();
-    
+
     // Initialize logger
-    env_logger::init();
-    //.filter_module("async_io", log::LevelFilter::Off)
-    //.filter_module("async_std", log::LevelFilter::Off)
-    //.filter_module("polling", log::LevelFilter::Off)
-    //.filter_module("tracing", log::LevelFilter::Off)
-    //.filter_module("sqlx", log::LevelFilter::Off)
-    //.filter_module("tower_http", log::LevelFilter::Off)
-    //.filter_module("tower::buffer::worker", log::LevelFilter::Off)
-    //.filter_module("tower::buffer::service", log::LevelFilter::Off)
-    //.filter_module("hyper", log::LevelFilter::Off)
-    //.init();
+    env_logger::builder()
+        .filter_module("async_io", log::LevelFilter::Off)
+        .filter_module("async_std", log::LevelFilter::Off)
+        .filter_module("polling", log::LevelFilter::Off)
+        .filter_module("tracing", log::LevelFilter::Off)
+        .filter_module("sqlx", log::LevelFilter::Off)
+        .filter_module("tower_http", log::LevelFilter::Off)
+        .filter_module("tower::buffer::worker", log::LevelFilter::Off)
+        .filter_module("tower::buffer::service", log::LevelFilter::Off)
+        .filter_module("hyper", log::LevelFilter::Off)
+        .init();
 
     // Parse command-line arguments and environment variables to create a Config instance
     let config = Config::parse();
@@ -42,7 +38,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .map_err(|e| format!("Failed to connect to the database: {}", e))?;
 
     // Perform database migrations
-    let migration_path = if var("RUNNING_IN_DOCKER").unwrap_or_else(|_| "false".to_string()).parse::<bool>().unwrap_or(false) {
+    let migration_path = if var("RUNNING_IN_DOCKER")
+        .unwrap_or_else(|_| "false".to_string())
+        .parse::<bool>()
+        .unwrap_or(false)
+    {
         Path::new("migrations")
     } else {
         Path::new("..\\migrations\\")
@@ -62,8 +62,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         token: Some(config.ipinfo_token.to_string()),
         ..Default::default()
     };
-    let ipinfo_client = IpInfo::new(ipinfo_config)
-        .map_err(|e| format!("Failed to create IpInfo client: {}", e))?;
+    let ipinfo_client =
+        IpInfo::new(ipinfo_config).map_err(|e| format!("Failed to create IpInfo client: {}", e))?;
 
     // Setup actor
     let brute_system = BruteSystem::new_brute(db, ipinfo_client).await;
@@ -71,15 +71,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Load TLS configuration
     let tls_config = RustlsConfig::from_pem_file(
-        PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("certs").join("cert.pem"),
-        PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("certs").join("key.pem"),
+        PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+            .join("certs")
+            .join("cert.pem"),
+        PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+            .join("certs")
+            .join("key.pem"),
     )
     .await
     .map_err(|e| format!("Failed to load TLS configuration: {}", e))?;
 
     // Serve TLS
-    serve_tls(brute_actor, tls_config, axum_server::Handle::new()).await
+    serve_tls(brute_actor, tls_config, axum_server::Handle::new())
+        .await
         .map_err(|e| format!("Failed to start TLS server: {}", e))?;
-
+    // use serve(actor) if you don't want to use tls.
     Ok(())
 }
