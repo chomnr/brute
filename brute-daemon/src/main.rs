e16e0c7a960c266798839868b1fb4a00ac0395e1
chomnr <zeljkovranjes@outlook.com>
2024-08-02 17:45:38 -0400

    basic daemon /w ssh

--- brute-daemon/src/main.rs
+++ brute-daemon/src/main.rs
@@ -3,11 +3,35 @@ use protocol::ssh::listen_on_ssh;
 mod protocol;
 mod endpoint;
 
+use std::fs::File;
+
+use daemonize::Daemonize;
+
+#[cfg(target_os = "windows")]
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     dotenvy::dotenv().unwrap();
-    //let d = Daemonize::new();
-    //println!("Hello, world!");
-    listen_on_ssh().await?;
+    Ok(())
+}
+
+#[cfg(target_os = "linux")]
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    dotenvy::dotenv().unwrap();
+    let daemonize = Daemonize::new()
+        .pid_file("/tmp/bruted.pid")
+        .chown_pid_file(true)
+        .umask(0o777)
+        .group("brute")
+        .working_directory("/tmp")
+        .stdout(File::create("/tmp/brute_daemon.stdout").unwrap())
+        .stderr(File::create("/tmp/brute_daemon.stderr").unwrap()); 
+    match daemonize.start() {
+        Ok(_) => {
+            println!("Success, daemonized");
+            listen_on_ssh().await?;
+        },
+        Err(e) => eprintln!("Error, {}", e),
+    }
     Ok(())
 }
